25ae81a79ce69deeeb9be174caad4c30ca9c8c84
Jasmin-Mediani <molihua1992@gmail.com>
2020-03-02 21:00:39 +0100

    risolto problema di sovrascrizione timer (se premuto play più volte)

--- js/main.js
+++ js/main.js
@@ -20,14 +20,16 @@ $(document).ready(function () {
             //questi sono i pallini
             var pallinoAttivo = $(".slider-nav i.active");
             var prossimoPallino = $(".slider-nav i.active").next();
+
+            imgAttiva.removeClass("active");
+            prossimaImg.addClass("active");
+
+
+            pallinoAttivo.removeClass("active");
+            prossimoPallino.addClass("active");
         }
 
-        imgAttiva.removeClass("active");
-        prossimaImg.addClass("active");
 
-
-        pallinoAttivo.removeClass("active");
-        prossimoPallino.addClass("active");
     }
 
 
@@ -50,36 +52,57 @@ $(document).ready(function () {
             //questi sono i pallini 
             var pallinoAttivo = $(".slider-nav i.active");
             var precedentePallino = $(".slider-nav i.active").prev();
+
+            imgAttiva.removeClass("active");
+            precedenteImg.addClass("active");
+
+            pallinoAttivo.removeClass("active");
+            precedentePallino.addClass("active");
         }
 
 
-        imgAttiva.removeClass("active");
-        precedenteImg.addClass("active");
 
-        pallinoAttivo.removeClass("active");
-        precedentePallino.addClass("active");
     }
 
 
 
     // Slider automatico che cambia ogni 3 secondi. Devo assegnare una variabile ("loop") a "setInterval(nextImage, 3000)" così da poterla richiamare come parametro quando faccio clearInterval:
 
-    var loop;
+    // var loop;
 
+    // $(".play-button").click(function () {
+    //     loop = setInterval(nextImage, 3000);
+    // });
+
+
+    // $(".pause-button").click(function () {
+    //     clearInterval(loop);
+    // });
+
+    // in questo modo però se clicco più volte su play si sovrascrive il loop, va tutto accelerato perché si creano più timer e il tasto pausa cancella solo l'ultimo timer creato. 
+    // Correzione a questo problema:   
+
+
+    var idTimer = null; //inizializzo la variabile idTimer con null;
     $(".play-button").click(function () {
-        loop = setInterval(nextImage, 3000);
+
+        if (idTimer === null) { //si controlla che non ci siano altri timer in esecuzione; 
+            idTimer = setInterval(nextImage, 3000); //se non ci sono, crea un timer e assegna il suo ID a idTimer
+        }
+
     });
+
 
 
     $(".pause-button").click(function () {
-        clearInterval(loop);
+        clearInterval(idTimer);
+        idTimer = null; //rimetto idTimer a null così posso rifare play dopo che è stato premuto pausa perché è stato eliminato il timer in esecuzione.  
     });
-
-
-
 
 
 
 
 
 });
+
+
